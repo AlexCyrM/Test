fde8f7e9aeaf021ed825d56db95381ff19b264ac
AlexCyrM <94937564+AlexCyrM@users.noreply.github.com>
2021-11-23 18:41:05 -0600

    Added ;

--- Hain.java
+++ Hain.java
@@ -1,7 +1,7 @@
 public class Hain {
   public static void main(String[] args) {
     String greeting = "Hello World";
-    String meanness = "Go Away World"
+    String meanness = "Go Away World";
     boolean amicable = true;
     if (amicable) {
       System.out.println(greeting);
