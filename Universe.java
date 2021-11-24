fe8f0d6d6ddfe0a36b06193fb8ce5eaea10dda63
AlexCyrM <94937564+AlexCyrM@users.noreply.github.com>
2021-11-23 20:47:07 -0600

    Create Universe.java

---
+++ Universe.java
@@ -0,0 +1,15 @@
+public class Universe {
+  public float curvature;
+  public int galaxyPop;
+  public long age;
+  public final int dimensionality;
+  public double habitability;
+  
+  public Universe(float _curvature, int _galaxyPop, long _age, int _dimensionality, double _habitability) {
+    curvature = _curvature;
+    galaxyPop = _galaxyPop;
+    age = _age;
+    dimensionality = _dimensionality;
+    habitability = _habitability;
+  } 
+}
