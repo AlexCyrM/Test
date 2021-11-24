c7f4e968d9f23c0f4afe6e4a572269d7f2d50111
AlexCyrM <94937564+AlexCyrM@users.noreply.github.com>
2021-11-23 18:58:14 -0600

    Made Amicability random

--- Hain.java
+++ Hain.java
@@ -2,8 +2,9 @@ public class Hain {
   public static void main(String[] args) {
     String greeting = "Hello World";
     String meanness = "Go Away World";
-    boolean amicable = true;
-    if (amicable) {
+    float currentAmicab = (float) Math.random();
+    float amicabThresh = 0.3
+    if (currentAmicab > amicabThresh) {
       System.out.println(greeting);
     } else {
       System.out.println(meanness);
