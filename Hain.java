84ab0c1d9acf206bb0574cd3644490e8e921c0cc
AlexCyrM <94937564+AlexCyrM@users.noreply.github.com>
2021-11-23 19:43:08 -0600

    Did update Jack suggested but on this brack

--- Hain.java
+++ Hain.java
@@ -1,13 +1,13 @@
 public class Hain {
+  private static final String GREETING = "Hello World";
+  private static final String MEANNESS = "Go Away World";
   public static void main(String[] args) {
-    String greeting = "Hello World";
-    String meanness = "Go Away World";
     float currentAmicab = (float) Math.random();
     float amicabThresh = 0.3
     if (currentAmicab > amicabThresh) {
-      System.out.println(greeting);
+      System.out.println(GREETING);
     } else {
-      System.out.println(meanness);
+      System.out.println(MEANNESS);
     }
   }
 }
