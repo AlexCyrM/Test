3ab09cfb6f2b952700fc7342cafdb8f2e4bd49e0
AlexCyrM <94937564+AlexCyrM@users.noreply.github.com>
2021-11-23 18:35:25 -0600

    Made it mean

--- Hain.java
+++ Hain.java
@@ -1,9 +1,12 @@
 public class Hain {
   public static void main(String[] args) {
     String greeting = "Hello World";
+    String meanness = "Go Away World"
     boolean amicable = true;
     if (amicable) {
       System.out.println(greeting);
+    } else {
+      System.out.println(meanness);
     }
   }
 }
