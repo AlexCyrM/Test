573d02faa700bfab10f0f9fbbf8aa2f9185e5b35
AlexCyrM <94937564+AlexCyrM@users.noreply.github.com>
2021-11-23 20:32:14 -0600

    Added expression and creativity and fixed floats

--- Hain.java
+++ Hain.java
@@ -2,12 +2,17 @@ public class Hain {
   private static final String GREETING = "Hello World";
   private static final String MEANNESS = "Go Away World";
   public static void main(String[] args) {
-    float currentAmicab = (float) Math.random();
-    float amicabThresh = 0.3
+    float curCharisma = (float) Math.random();
+    float chaThresh = 0.09f;
+    float currentAmicab = (float) Math.random(); //generates a number representing how amicable the machine is
+    float amicabThresh = 0.3f; //how amicable it needs to be to greet
+    double creativity = Math.random(); //how much the machine creates
     if (currentAmicab > amicabThresh) {
       System.out.println(GREETING);
-    } else {
+    } else if (curCharisma > chaThresh) {
       System.out.println(MEANNESS);
+    } else {
     }
+    System.out.println("... anyhow I built something");
   }
 }
