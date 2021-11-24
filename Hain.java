08b7204a2a841873ab7bc63bdecd1534fae20e9b
AlexCyrM <94937564+AlexCyrM@users.noreply.github.com>
2021-11-23 19:41:54 -0600

    Did some stuff Jack suggested

--- Hain.java
+++ Hain.java
@@ -1,12 +1,12 @@
 public class Hain {
+  private static final String GREETING = "Hello World";
+  private static final String MEANNESS = "Go Away World";
   public static void main(String[] args) {
-    String greeting = "Hello World";
-    String meanness = "Go Away World";
     boolean amicable = true;
     if (amicable) {
-      System.out.println(greeting);
+      System.out.println(GREETING);
     } else {
-      System.out.println(meanness);
+      System.out.println(MEANNESS);
     }
   }
 }
